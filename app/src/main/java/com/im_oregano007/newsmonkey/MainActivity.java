29776833ed32a0911816442a0ce2522215b10480
iMoregano007 <imgauravs84@gmail.com>
2023-08-26 23:03:23 +0530

    NewsMonkey project...

---
+++ app/src/main/java/com/im_oregano007/newsmonkey/MainActivity.java
@@ -0,0 +1,14 @@
+package com.im_oregano007.newsmonkey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
